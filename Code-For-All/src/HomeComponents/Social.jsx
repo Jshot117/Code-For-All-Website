44443fcde07d30b2bc77c898ba1c6f22512384bf
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-05-29 04:18:17 -0400

    updated Social component

--- Code-For-All/src/HomeComponents/Social.jsx
+++ Code-For-All/src/HomeComponents/Social.jsx
@@ -1,16 +1,32 @@
-import discord from '../assets/discord.png'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-function Social() {
-    function handleClick() {
-        window.open('https://discord.gg/s8jwd4Es')
-    }
-
+const Social = () => {
     return (
-        <div className="flex-col justify-center items-center ">
-            <h1 className="text-center">Join our Discord</h1>
-            <img onClick={handleClick} src={discord}
-                className="mx-auto w-11 h-9 cursor-pointer hover:scale-110 ease-in-out duration-400"></img>
+        <div className = "flex-row">
+            <div className="flex items-center justify-between p-4 bg-white border-t border-gray-200">
+                {/* Logo Section */}
+                <div className="flex items-center mr-28">
+                    <img src="src/assets/logo.png" alt="Code For All Logo" className="h-8" />
+                    <span className="ml-2 text-xl font-bold">CODE FOR ALL</span>
+                </div>
+
+                <div className="flex space-x-4 mr-auto">
+                    <a href="https://www.linkedin.com/company/code-for-all-qc/" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} size="lg" className="text-gray-600 hover:text-blue-600" />
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faInstagram} size="lg" className="text-gray-600 hover:text-pink-600" />
+                    </a>
+                </div>
+            </div>
+            <div className="flex items-center p-4">
+                <h1 className = "font-bold text-xl" >Contact Us:</h1>
+                <p className = "pl-2">emailcodeforall@gmail.com</p>
+            </div>
         </div>
-    )
-}
+    );
+};
+
 export default Social;
