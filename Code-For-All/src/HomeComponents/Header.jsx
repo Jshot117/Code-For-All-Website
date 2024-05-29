d997777a464df0bea95eefa2f1cd7298703f33a2
justinespinal <espinaljustin03@gmail.com>
2024-05-29 00:45:09 -0400

    finished header

--- Code-For-All/src/HomeComponents/Header.jsx
+++ Code-For-All/src/HomeComponents/Header.jsx
@@ -1,13 +1,15 @@
+import logo from "../assets/logo.png";
+
 function Header() {
     return (
-        <header className ="bg-gray-500 text-white p-4">
+        <header className ="text-black p-4">
             <div className="container mx-auto flex items-center">
-                <h1 className="text-2xl font-bold">Code For All</h1>
-                <nav className="ml-auto">
+                <img src={logo} alt="Code for All logo" className = "max-w-28 hover:scale-105 ease-in-out duration-300 "></img>
+                <nav className="mx-auto">
                     <ul className="flex space-x-4">
-                        <li className="p-2 hover:bg-gray-700">Home</li>
-                        <li className="p-2 hover:bg-gray-700">About</li>
-                        <li className="p-2 hover:bg-gray-700">More Events</li>
+                        <li className="p-2 hover:scale-105 ease-in-out duration-300 hover:underline cursor-pointer text-xl">Home</li>
+                        <li className="p-2 hover:scale-105 ease-in-out duration-300 hover:underline cursor-pointer text-xl">About</li>
+                        <li className="p-2 hover:scale-105 ease-in-out duration-300 hover:underline cursor-pointer text-xl">More Events</li>
                     </ul>
                 </nav>
             </div>
