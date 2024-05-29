6d4df89e39ccf6f54268ea67da53406f2542df64
justinespinal <espinaljustin03@gmail.com>
2024-05-29 15:04:49 -0400

    carousel

--- Code-For-All/src/HomeComponents/Board.jsx
+++ Code-For-All/src/HomeComponents/Board.jsx
@@ -1,12 +1,67 @@
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import BoardCard from './BoardCard';
+import justin from '../assets/justin.jpg';
+import thomas from '../assests/thomas.jpeg';
 
 function Board(){
     return(
-        <div>
-
-        </div>
+        <Carousel
+            additionalTransfrom={0}
+            arrows
+            autoPlaySpeed={3000}
+            centerMode={false}
+            className=""
+            containerClass="container-with-dots"
+            dotListClass=""
+            draggable
+            focusOnSelect={false}
+            infinite
+            itemClass=""
+            keyBoardControl
+            minimumTouchDrag={80}
+            pauseOnHover
+            renderArrowsWhenDisabled={false}
+            renderButtonGroupOutside={false}
+            renderDotsOutside={false}
+            responsive={{
+                desktop: {
+                breakpoint: {
+                    max: 3000,
+                    min: 1024
+                },
+                items: 3,
+                partialVisibilityGutter: 40
+                },
+                mobile: {
+                breakpoint: {
+                    max: 464,
+                    min: 0
+                },
+                items: 1,
+                partialVisibilityGutter: 30
+                },
+                tablet: {
+                breakpoint: {
+                    max: 1024,
+                    min: 464
+                },
+                items: 2,
+                partialVisibilityGutter: 30
+                }
+            }}
+            rewind={false}
+            rewindWithAnimation={false}
+            rtl={false}
+            shouldResetAutoplay
+            showDots={false}
+            sliderClass=""
+            slidesToSlide={1}
+            swipeable
+        >
+            <BoardCard image={justin} name="Justin Espinal" role = "President & Founder" description="I love to collect figures!"/>
+            <BoardCard image={thomas} name="Thomas Soupionis" role = "Vice President" description="" />
+        </Carousel>
     )
 }
 
