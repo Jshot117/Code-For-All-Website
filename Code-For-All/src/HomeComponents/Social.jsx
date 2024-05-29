f3ef592f895375294fc2959825d107ab3b5d96eb
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-05-29 04:55:43 -0400

    Fixed allignment

--- Code-For-All/src/HomeComponents/Social.jsx
+++ Code-For-All/src/HomeComponents/Social.jsx
@@ -4,26 +4,25 @@ import { faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Social = () => {
     return (
-        <div className = "flex-row">
-            <div className="flex items-center justify-between p-4 bg-white border-t border-gray-200">
-                {/* Logo Section */}
-                <div className="flex items-center mr-28">
-                    <img src="src/assets/logo.png" alt="Code For All Logo" className="h-8" />
+        <div className="container mx-auto flex-row items-center">
+            <div className="flex  items-center justify-between p-4 bg-white border-t border-gray-200">
+                <div className="flex  items-center mr-20 ">
+                    <img src="src/assets/logo.png" alt="Code For All Logo" className="hover:scale-105 h-8" />
                     <span className="ml-2 text-xl font-bold">CODE FOR ALL</span>
                 </div>
 
-                <div className="flex space-x-4 mr-auto">
-                    <a href="https://www.linkedin.com/company/code-for-all-qc/" target="_blank" rel="noopener noreferrer">
+                <div className="flex space-x-4 mr-auto ">
+                    <a className="hover:scale-105" href="https://www.linkedin.com/company/code-for-all-qc/" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faLinkedin} size="lg" className="text-gray-600 hover:text-blue-600" />
                     </a>
-                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                    <a className="hover:scale-105" href="https://www.instagram.com/codeforall_qc/" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faInstagram} size="lg" className="text-gray-600 hover:text-pink-600" />
                     </a>
                 </div>
             </div>
             <div className="flex items-center p-4">
-                <h1 className = "font-bold text-xl" >Contact Us:</h1>
-                <p className = "pl-2">emailcodeforall@gmail.com</p>
+                <h1 className="font-bold text-xl " >Contact Us:</h1>
+                <p className="pl-2 ">emailcodeforall@gmail.com</p>
             </div>
         </div>
     );
