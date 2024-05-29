637702b3e297fe6efdae5672f0796784960a5729
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-05-29 04:17:22 -0400

    formatted Header component

--- Code-For-All/src/HomeComponents/Header.jsx
+++ Code-For-All/src/HomeComponents/Header.jsx
@@ -2,9 +2,9 @@ import logo from "../assets/logo.png";
 
 function Header() {
     return (
-        <header className ="text-black p-4">
+        <header className="text-black p-4">
             <div className="container mx-auto flex items-center">
-                <img src={logo} alt="Code for All logo" className = "max-w-28 hover:scale-105 ease-in-out duration-300 "></img>
+                <img src={logo} alt="Code for All logo" className="max-w-28 hover:scale-105 ease-in-out duration-300 "></img>
                 <nav className="mx-auto">
                     <ul className="flex space-x-4">
                         <li className="p-2 hover:scale-105 ease-in-out duration-300 hover:underline cursor-pointer text-xl">Home</li>
