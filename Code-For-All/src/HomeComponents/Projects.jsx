65133f39afeadc66ef1a7659c83143e9abc4eea7
justinespinal <espinaljustin03@gmail.com>
2024-05-30 03:31:59 -0400

    fixed j in Projects

--- Code-For-All/src/HomeComponents/Projects.jsx
+++ Code-For-All/src/HomeComponents/Projects.jsx
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCard"
 function Projects(){
     return (
         <div className="flex flex-col">
-            <h1 className="p-0 
+            <h1 className="p-4 
                             bg-clip-text text-transparent bg-gradient-to-r from-black to-purple-400 
                             text-6xl md:text-7xl font-bold mb-4 font-mono text-center"
                             >Code for All Projects</h1>
