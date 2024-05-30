aef25d0fa7582c40f7a908dd543f177e00dca447
justinespinal <espinaljustin03@gmail.com>
2024-05-30 01:59:42 -0400

    final board

--- Code-For-All/src/HomeComponents/Board.jsx
+++ Code-For-All/src/HomeComponents/Board.jsx
@@ -1,7 +1,7 @@
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import BoardCard from './BoardCard';
-import justin from '../assets/justin.jpg';
+import Justin from '../assets/justin.jpg';
 import Thomas from '../assets/thomas.jpeg';
 import Jonnathan from '../assets/Jonnathan.jpg'
 import Romel from '../assets/Romel.jpg'
@@ -13,13 +13,18 @@ import Daniel from '../assets/daniel.jpg'
 import Rakib from '../assets/rakib.jpg'
 import Yonghao from '../assets/yonghao.png'
 import Patryk from '../assets/patryk.jpg'
+import Lauren from '../assets/lauren.jpg'
 import Feng from '../assets/feng.jpeg'
+import Shaine from '../assets/shaine.jpeg'
+import Dennis from '../assets/dennis.jpeg'
+import Oliver from '../assets/oliver.jpg'
+import Athor from '../assets/athor.png'
 
 
 function Board(){
     return(
         <div className = "mt-36 text-center">
-            <h1 className="hover:scale-105 ease-in-out duration-300 p-0 
+            <h1 className="p-0 
                             bg-clip-text text-transparent bg-gradient-to-r from-black to-purple-400 
                             text-6xl md:text-7xl font-bold mb-4 font-mono">Our Board</h1>
             <Carousel
@@ -75,7 +80,7 @@ function Board(){
                 slidesToSlide={1}
                 swipeable
             >
-                <BoardCard image={justin} name="Justin Espinal" role = "President & Founder" description="I love to collect figures!"/>
+                <BoardCard image={Justin} name="Justin Espinal" role = "President & Founder" description="I love to collect figures!"/>
                 <BoardCard image={Thomas} name="Thomas Soupionis" role = "Vice-President" description="I like buying sneakers"/>
                 <BoardCard image={Eric} name="Eric Salazar" role = "Treasurer" description="I have an orange cat named bibi"/>
                 <BoardCard image={Amart} name="Andrew Martin" role = "Secretary & Advisor" description="I used to study theatre"/>
@@ -88,8 +93,11 @@ function Board(){
                 <BoardCard image={Yonghao} name="Yonghao Lin" role = "Tutor" description="I like cats, crocheting, video games (Minecraft), and the gym"/>
                 <BoardCard image={Patryk} name="Patryk Stypulkowski" role = "Tutor" description="I hate front end!"/>
                 <BoardCard image={Feng} name="Fengsheng Chen" role = "Tutor" description="Souls game enjoyer"/>
-                <BoardCard image={Patryk} name="Shaine Lomenario" role = "Marketing" description="I hate front end!"/>
-
+                <BoardCard image={Lauren} name="Lauren Mistretta" role = "Advisor" description="I enjoy playing video games and drawing!"/>
+                <BoardCard image={Shaine} name="Shaine Lomenario" role = "Marketing" description="I like to play video games and the piano"/>
+                <BoardCard image={Dennis} name="Dennis Do" role = "Marketing" description="I love working out, playing sports, and meeting new people"/>
+                <BoardCard image={Athor} name="Andrew Thoral" role = "Marketing" description="Blank for now"/>
+                <BoardCard image={Oliver} name="Oliver" role = "Mascot" description="Meow"/>
             </Carousel>
         </div>
         
