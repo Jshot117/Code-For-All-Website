4c69abf833e68628c3fb17ee8b5877bcdfe5f8e8
justinespinal <espinaljustin03@gmail.com>
2024-06-01 12:16:31 -0400

    added past board

--- Code-For-All/src/HomeComponents/Board.jsx
+++ Code-For-All/src/HomeComponents/Board.jsx
@@ -2,6 +2,7 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import BoardCard from './BoardCard';
 import {BOARDINFO} from '../BoardInfo'
+import {PASTBOARDINFO} from '../PastBoard'
 import { forwardRef, useEffect, useState } from 'react';
 
 const Board = forwardRef((props, ref) => {
@@ -21,15 +22,17 @@ const Board = forwardRef((props, ref) => {
             <Carousel
                 additionalTransfrom={0}
                 arrows
-                autoPlaySpeed={3000}
-                centerMode={!isMobile}
+                autoPlay
+                autoPlaySpeed={4000}
+                centerMode={false}
                 className=""
                 containerClass="container-with-dots"
+                customTransition="all 1s linear"
                 dotListClass=""
                 draggable
                 focusOnSelect={false}
                 infinite
-                itemClass="flex justify-center"
+                itemClass=""
                 keyBoardControl
                 minimumTouchDrag={80}
                 pauseOnHover
@@ -37,57 +40,105 @@ const Board = forwardRef((props, ref) => {
                 renderButtonGroupOutside={false}
                 renderDotsOutside={false}
                 responsive={{
-                    desktop: {
-                        breakpoint: {
-                            max: 3000,
-                            min: 1500
-                        },
-                        items: 4,
-                        partialVisibilityGutter: 40
+                  desktop: {
+                    breakpoint: {
+                      max: 3000,
+                      min: 1024
                     },
-                    smallDesktop: {
-                        breakpoint: {
-                            max: 1500,
-                            min: 1024
-                        },
-                        items: 3,
-                        partialVisibilityGutter: 30
+                    items: 5,
+                    partialVisibilityGutter: 40
+                  },
+                  mobile: {
+                    breakpoint: {
+                      max: 464,
+                      min: 0
                     },
-                    mobile: {
-                        breakpoint: {
-                            max: 480,
-                            min: 0
-                        },
-                        items: 1,
-                        partialVisibilityGutter: 30
-                        },
-                    largeMobile: {
-                        breakpoint: {
-                            max: 768,
-                            min: 480
-                        },
-                        items: 1,
-                        partialVisibilityGutter: 30
+                    items: 1,
+                    partialVisibilityGutter: 30
+                  },
+                  tablet: {
+                    breakpoint: {
+                      max: 1024,
+                      min: 464
                     },
-                    tablet: {
-                        breakpoint: {
-                            max: 1024,
-                            min: 768
-                        },
-                        items: 2,
-                        partialVisibilityGutter: 30
-                    }
+                    items: 2,
+                    partialVisibilityGutter: 30
+                  }
                 }}
                 rewind={false}
                 rewindWithAnimation={false}
                 rtl={false}
                 shouldResetAutoplay
-                showDots={true}
+                showDots={false}
                 sliderClass=""
                 slidesToSlide={1}
                 swipeable
+                transitionDuration={1000}
             >
                 {BOARDINFO.map((member) => (
+                    <BoardCard image = {member.image} name = {member.name} role = {member.role} description={member.description} link = {member.link}/>
+                ))}
+            </Carousel>
+            <br/>
+            <h1 className="p-0 
+                            bg-clip-text text-transparent bg-gradient-to-r from-black to-purple-400 
+                            text-6xl md:text-7xl font-bold mb-4 font-mono">Past Board</h1>
+            <Carousel
+                additionalTransfrom={0}
+                arrows
+                autoPlay
+                autoPlaySpeed={4000}
+                centerMode={false}
+                className=""
+                containerClass="container-with-dots"
+                customTransition="all 1s linear"
+                dotListClass=""
+                draggable
+                focusOnSelect={false}
+                itemClass=""
+                keyBoardControl
+                minimumTouchDrag={80}
+                pauseOnHover
+                renderArrowsWhenDisabled={false}
+                renderButtonGroupOutside={false}
+                renderDotsOutside={false}
+                responsive={{
+                  desktop: {
+                    breakpoint: {
+                      max: 3000,
+                      min: 1024
+                    },
+                    items: 4,
+                    partialVisibilityGutter: 40
+                  },
+                  mobile: {
+                    breakpoint: {
+                      max: 464,
+                      min: 0
+                    },
+                    items: 1,
+                    partialVisibilityGutter: 30
+                  },
+                  tablet: {
+                    breakpoint: {
+                      max: 1024,
+                      min: 464
+                    },
+                    items: 2,
+                    partialVisibilityGutter: 30
+                  }
+                }}
+                rewind={false}
+                rewindWithAnimation={false}
+                rtl={false}
+                shouldResetAutoplay
+                showDots={false}
+                sliderClass=""
+                slidesToSlide={1}
+                swipeable
+                transitionDuration={1000}
+            >
+                {PASTBOARDINFO.map((member) => (
                     <BoardCard image = {member.image} name = {member.name} role = {member.role} description={member.description} link = {member.link}/>
                 ))}
             </Carousel>
---
+++ Code-For-All/src/PastBoard.jsx
@@ -0,0 +1,32 @@
+import Avi from "./assets/avi.jpeg"
+import Mo from "./assets/mo.jpeg"
+import Jorge from "./assets/jorge.jpeg"
+export const PASTBOARDINFO = [
+    {
+        image: Avi,
+        name:"Avraham Uvyadov",
+        role: "Secretary",
+        description: "Blank for now",
+        link: "https://www.linkedin.com/in/avraham-uvaydov/"
+    },
+    {
+        image: Jorge,
+        name:"Jorge Robles",
+        role: "Workshop Developer",
+        description: "Blank for now",
+        link: "https://www.linkedin.com/in/jorge-d-robles/"
+    },
+    {
+        image: Avi,
+        name:"Huzayfah (Hayyan) Awan",
+        role: "Workshop Developer",
+        description: "Blank for now",
+        link: "https://www.linkedin.com/in/huzayfah-awan-172729241/"
+    },{
+        image: Mo,
+        name:"Mohammad Mustafa",
+        role: "Event Manager",
+        description: "Blank for now",
+        link: "https://www.linkedin.com/in/mohammad-mustafa786/"
+    }
+];
